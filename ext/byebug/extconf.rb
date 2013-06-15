f5a0b1dcaac0bcd2384eb6288912acd7a8a44e53
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-06-15 18:21:30 +0200

    Check for GCC before adding GCC-specific flags

--- ext/byebug/extconf.rb
+++ ext/byebug/extconf.rb
@@ -1,12 +1,16 @@
 require 'mkmf'
 
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+
 if RUBY_VERSION < "2.0"
   STDERR.print("Ruby version is too old\n")
   exit(1)
 end
 
-$CFLAGS = '-Wall -Werror'
-$CFLAGS += ' -g3' if ENV['debug']
+if RbConfig::MAKEFILE_CONFIG['CC'] =~ /gcc/
+  $CFLAGS = '-Wall -Werror'
+  $CFLAGS += ' -g3' if ENV['debug']
+end
 
 dir_config("ruby")
 create_makefile("byebug")
