7fe61eb3e7312d7a20de635f3332129b780d3118
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-08-29 23:55:41 +0200

    Remove unnecessary nesting in a test

--- test/stepping_test.rb
+++ test/stepping_test.rb
@@ -4,114 +4,106 @@ class TestStepping < TestDsl::TestCase
 
   describe 'Next Command' do
 
-    describe 'Usual mode' do
+    describe 'method call behaviour' do
+      before { enter 'break 10', 'cont' }
 
-      describe 'method call behaviour' do
-        before { enter 'break 10', 'cont' }
+      it 'must leave on the same line by default' do
+        enter 'next'
+        debug_file('stepping') { $state.line.must_equal 10 }
+      end
 
-        it 'must leave on the same line by default' do
+      it 'must go to the next line if forced by "plus" sign' do
+        enter 'next+'
+        debug_file('stepping') { $state.line.must_equal 11 }
+      end
+
+      it 'must leave on the same line if forced by "minus" sign' do
+        enter 'next-'
+        debug_file('stepping') { $state.line.must_equal 10 }
+      end
+
+      describe 'when forcestep is set' do
+        temporary_change_hash Byebug.settings, :forcestep, true
+
+        it 'must go to the next line' do
           enter 'next'
-          debug_file('stepping') { $state.line.must_equal 10 }
-        end
-
-        it 'must go to the next line if forced by "plus" sign' do
-          enter 'next+'
           debug_file('stepping') { $state.line.must_equal 11 }
         end
 
-        it 'must leave on the same line if forced by "minus" sign' do
+        it 'must go to the next line (by shortcut)' do
+          enter 'n'
+          debug_file('stepping') { $state.line.must_equal 11 }
+        end
+
+        it 'must go the specified number of lines forward by default' do
+          enter 'next 2'
+          debug_file('stepping') { $state.line.must_equal 21 }
+        end
+
+        it 'must ignore it if "minus" is specified' do
           enter 'next-'
           debug_file('stepping') { $state.line.must_equal 10 }
         end
-
-        describe 'when forcestep is set' do
-          temporary_change_hash Byebug.settings, :forcestep, true
-
-          it 'must go to the next line' do
-            enter 'next'
-            debug_file('stepping') { $state.line.must_equal 11 }
-          end
-
-          it 'must go to the next line (by shortcut)' do
-            enter 'n'
-            debug_file('stepping') { $state.line.must_equal 11 }
-          end
-
-          it 'must go the specified number of lines forward by default' do
-            enter 'next 2'
-            debug_file('stepping') { $state.line.must_equal 21 }
-          end
-
-          it 'must ignore it if "minus" is specified' do
-            enter 'next-'
-            debug_file('stepping') { $state.line.must_equal 10 }
-          end
-        end
       end
+    end
 
-      describe 'block behaviour' do
-        before { enter 'break 21', 'cont' }
+    describe 'block behaviour' do
+      before { enter 'break 21', 'cont' }
 
-        it 'must step over blocks' do
-          enter 'next'
-          debug_file('stepping') { $state.line.must_equal 25 }
-        end
+      it 'must step over blocks' do
+        enter 'next'
+        debug_file('stepping') { $state.line.must_equal 25 }
       end
-
     end
   end
 
   describe 'Step Command' do
 
-    describe 'Usual mode' do
+    describe 'method call behaviour' do
+      before { enter 'break 10', 'cont' }
 
-      describe 'method call behaviour' do
-        before { enter 'break 10', 'cont' }
+      it 'must leave on the same line if forced by a setting' do
+        enter 'step'
+        debug_file('stepping') { $state.line.must_equal 10 }
+      end
 
-        it 'must leave on the same line if forced by a setting' do
+      it 'must go to the step line if forced to do that by "plus" sign' do
+        enter 'step+'
+        debug_file('stepping') { $state.line.must_equal 11 }
+      end
+
+      it 'must leave on the same line if forced to do that by "minus" sign' do
+        enter 'step-'
+        debug_file('stepping') { $state.line.must_equal 10 }
+      end
+
+      describe 'when forcestep is set' do
+        temporary_change_hash Byebug.settings, :forcestep, true
+
+        it 'must go to the step line if forced by a setting' do
           enter 'step'
-          debug_file('stepping') { $state.line.must_equal 10 }
-        end
-
-        it 'must go to the step line if forced to do that by "plus" sign' do
-          enter 'step+'
           debug_file('stepping') { $state.line.must_equal 11 }
         end
 
-        it 'must leave on the same line if forced to do that by "minus" sign' do
-          enter 'step-'
-          debug_file('stepping') { $state.line.must_equal 10 }
+        it 'must go to the next line if forced by a setting (by shortcut)' do
+          enter 's'
+          debug_file('stepping') { $state.line.must_equal 11 }
         end
 
-        describe 'when forcestep is set' do
-          temporary_change_hash Byebug.settings, :forcestep, true
-
-          it 'must go to the step line if forced by a setting' do
-            enter 'step'
-            debug_file('stepping') { $state.line.must_equal 11 }
-          end
-
-          it 'must go to the next line if forced by a setting (by shortcut)' do
-            enter 's'
-            debug_file('stepping') { $state.line.must_equal 11 }
-          end
-
-          it 'must go the specified number of lines forward by default' do
-            enter 'step 2'
-            debug_file('stepping') { $state.line.must_equal 15 }
-          end
-        end
-      end
-
-      describe 'block behaviour' do
-        before { enter 'break 21', 'cont' }
-
-        it 'must step into blocks' do
-          enter 'step'
-          debug_file('stepping') { $state.line.must_equal 22 }
+        it 'must go the specified number of lines forward by default' do
+          enter 'step 2'
+          debug_file('stepping') { $state.line.must_equal 15 }
         end
       end
     end
-  end
 
+    describe 'block behaviour' do
+      before { enter 'break 21', 'cont' }
+
+      it 'must step into blocks' do
+        enter 'step'
+        debug_file('stepping') { $state.line.must_equal 22 }
+      end
+    end
+  end
 end
