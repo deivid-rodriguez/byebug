e8bbffd266462760bb8d9e62eece1b463ed1d738
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-04-01 19:29:53 +0200

    Fix require order issue
    Triggered when `byebug` used while pry-byebug installed.

--- lib/byebug/processor.rb
+++ lib/byebug/processor.rb
@@ -1,21 +1,18 @@
 require 'forwardable'
 require 'byebug/interface'
-require 'byebug/command'
 
-module Byebug
+# Should this be a mixin?
+class Byebug::Processor
+  attr_accessor :interface
 
-  # Should this be a mixin?
-  class Processor
-    attr_accessor :interface
+  extend Forwardable
+  def_delegators :@interface, :errmsg, :print
 
-    extend Forwardable
-    def_delegators :@interface, :errmsg, :print
-
-    def initialize(interface)
-      @interface = interface
-    end
+  def initialize(interface)
+    @interface = interface
   end
-
-  require 'byebug/processors/command_processor'
-  require 'byebug/processors/control_command_processor'
 end
+
+require 'byebug/command'
+require 'byebug/processors/command_processor'
+require 'byebug/processors/control_command_processor'
