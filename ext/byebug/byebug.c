408d5080ff80cfc856727f3d7cf02025ab58a22c
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-04-21 15:05:30 +0200

    Print debug output to stdout

--- ext/byebug/byebug.c
+++ ext/byebug/byebug.c
@@ -41,9 +41,9 @@ trace_print(rb_trace_arg_t *trace_arg, debug_context_t *dc)
     VALUE v_mid = rb_tracearg_method_id(trace_arg);
     const char *mid = NIL_P(v_mid) ? "" : rb_id2name(SYM2ID(v_mid));
 
-    for (i = 0; i < dc->calced_stack_size; i++) putc('-', stderr);
-    fprintf(stderr, "(%d)->[#%d] %s@%s:%d %s\n",
-                     dc->calced_stack_size, dc->thnum, event, path, line, mid);
+    for (i = 0; i < dc->calced_stack_size; i++) putc('-', stdout);
+    printf("(%d)->[#%d] %s@%s:%d %s\n",
+           dc->calced_stack_size, dc->thnum, event, path, line, mid);
   }
 }
 
