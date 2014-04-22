6faab156cccabf8f708c6da88493b3666fcb2044
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-04-22 05:42:56 +0200

    Fix autoeval setting

--- lib/byebug/settings/autoeval.rb
+++ lib/byebug/settings/autoeval.rb
@@ -1,12 +1,20 @@
 module Byebug
   class Autoeval < Setting
     def initialize
-      @value = true
+      EvalCommand.unknown = true
     end
 
     def help
       'If true, byebug will evaluate every unrecognized command. If false, ' \
       'need to use the `eval` command to evaluate stuff'
+    end
+
+    def value=(v)
+      EvalCommand.unknown = v
+    end
+
+    def value
+      EvalCommand.unknown
     end
   end
 
