40e1244d246daa7a0653ce10c33ef0aad1989682
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-08-01 18:22:26 +0200

    One file per command

---
+++ lib/byebug/commands/interrupt.rb
@@ -0,0 +1,25 @@
+module Byebug
+  class InterruptCommand < Command
+    self.allow_in_control     = true
+    self.allow_in_post_mortem = false
+
+    def regexp
+      /^\s*i(?:nterrupt)?\s*$/
+    end
+
+    def execute
+      context = Byebug.thread_context(Thread.main)
+      context.interrupt
+    end
+
+    class << self
+      def names
+        %w(interrupt)
+      end
+
+      def description
+        %{i|nterrupt\t interrupt the program}
+      end
+    end
+  end
+end
