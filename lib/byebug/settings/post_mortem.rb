544585374f2064add0462a82007345f945962e69
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-08-06 15:41:36 +0200

    Try to fix failure in travis

--- lib/byebug/settings/post_mortem.rb
+++ lib/byebug/settings/post_mortem.rb
@@ -21,12 +21,12 @@ module Byebug
   # Saves information about the unhandled exception and gives a byebug
   # prompt back to the user before program termination.
   #
-  def handle_post_mortem
+  def self.handle_post_mortem
     context = Byebug.raised_exception.__bb_context
     file    = Byebug.raised_exception.__bb_file
     line    = Byebug.raised_exception.__bb_line
     Byebug.handler.at_line(context, file, line)
   end
 
-  at_exit { handle_post_mortem if Byebug.post_mortem? }
+  at_exit { Byebug.handle_post_mortem if Byebug.post_mortem? }
 end
