90e87c9885d5f58a00b1b16270ac9ad302c68098
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-08-14 16:48:45 +0200

    Standarize debugger_alias test

--- test/debugger_alias_test.rb
+++ test/debugger_alias_test.rb
@@ -1,5 +1,5 @@
-module DebuggerAlias
-  class DebuggerAliasTest < Minitest::Test
+module Byebug
+  class DebuggerAliasTestCase < TestCase
     def test_aliases_debugger_to_byebug
       assert_equal Kernel.method(:byebug), Kernel.method(:debugger)
     end
