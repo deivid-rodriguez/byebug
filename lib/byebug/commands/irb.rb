6fd823af0f6bd0f7567e68e98b88f3cbe81c9d14
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-08-14 16:53:20 +0200

    Split pry and irb commands into separate files
    For consistency.

---
+++ lib/byebug/commands/irb.rb
@@ -0,0 +1,28 @@
+require 'irb'
+
+module Byebug
+  class IrbCommand < Command
+    def regexp
+      /^\s* irb \s*$/x
+    end
+
+    def execute
+      unless @state.interface.kind_of?(LocalInterface)
+        print "Command is available only in local mode.\n"
+        throw :debug_error
+      end
+
+      cont = IRB.start(__FILE__)
+    end
+
+    class << self
+      def names
+        %w(irb)
+      end
+
+      def description
+        %{irb\tstarts an Interactive Ruby (IRB) session.}
+      end
+    end
+  end
+end
---
+++ lib/byebug/commands/pry.rb
@@ -0,0 +1,33 @@
+begin
+  require 'pry'
+  has_pry = true
+rescue LoadError
+  has_pry = false
+end
+
+module Byebug
+  class PryCommand < Command
+    def regexp
+      /^\s* pry \s*$/x
+    end
+
+    def execute
+      unless @state.interface.kind_of?(LocalInterface)
+        print "Command is available only in local mode.\n"
+        throw :debug_error
+      end
+
+      get_binding.pry
+    end
+
+    class << self
+      def names
+        %w(pry)
+      end
+
+      def description
+        %{pry\tstarts a Pry session.}
+      end
+    end
+  end
+end if has_pry
