f8e5d52703c98bb9855612f134d42e8462d075f8
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-08-20 09:06:23 +0200

    Remove duplicated test

--- test/commands/debugger_alias_test.rb
+++
@@ -1,7 +0,0 @@
-module Byebug
-  class DebuggerAliasTestCase < TestCase
-    def test_aliases_debugger_to_byebug
-      assert_equal Kernel.method(:byebug), Kernel.method(:debugger)
-    end
-  end
-end
