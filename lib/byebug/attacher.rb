ab7b8d95c005d007363df0e221e9db236d3e2777
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-09-22 16:48:31 +0200

    Extract method to Byebug module

--- lib/byebug/attacher.rb
+++ lib/byebug/attacher.rb
@@ -1,6 +1,15 @@
 module Byebug
   # Stores program being debugged to make restarts possible
   PROG_SCRIPT = $PROGRAM_NAME unless defined?(PROG_SCRIPT)
+  #
+  # Enters byebug right before (or right after if _before_ is false) return
+  # events occur. Before entering byebug the init script is read.
+  #
+  def self.attach(steps_out, before)
+    start
+    run_init_script(StringIO.new)
+    current_context.step_out(steps_out, before)
+  end
 end
 
 #
@@ -9,14 +18,8 @@ end
 # Dropping a `byebug` call anywhere in your code, you get a debug prompt.
 #
 module Kernel
-  #
-  # Enters byebug right before (or right after if _before_ is false) return
-  # events occur. Before entering byebug the init script is read.
-  #
   def byebug(steps_out = 1, before = true)
-    Byebug.start
-    Byebug.run_init_script(StringIO.new)
-    Byebug.current_context.step_out(steps_out, before)
+    Byebug.attach(steps_out + 1, before)
   end
 
   alias_method :debugger, :byebug
