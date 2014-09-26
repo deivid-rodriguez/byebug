2e6312e5a0e489f0717183e266a20e19155139ed
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-09-26 13:08:32 +0200

    Move runner creation to test setup

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -1,11 +1,11 @@
 require 'byebug/runner'
 
 module Byebug
-
   class RunnerTest < TestCase
     def setup
       super
       @old_argv = ARGV
+      @runner = Byebug::Runner.new
     end
 
     def after
@@ -14,14 +14,14 @@ module Byebug
 
     def test_run_with_version_flag
       ARGV.replace(%w(--version))
-      Byebug::Runner.new.run
+      @runner.run
 
       check_output_includes(/#{Byebug::VERSION}/)
     end
 
     def test_run_with_help_flag
       ARGV.replace(%w(--help))
-      Byebug::Runner.new.run
+      @runner.run
 
       check_output_includes(/-d.*-I.*-q.*-s.*-x.*-m.*-r.*-R.*-t.*-v.*-h/m)
     end
@@ -29,7 +29,7 @@ module Byebug
     def test_run_with_remote_option_only_with_a_port_number
       ARGV.replace(%w(--remote 9999))
       Byebug.expects(:start_client)
-      Byebug::Runner.new.run
+      @runner.run
 
       check_output_includes(/Connecting to byebug server localhost:9999/)
     end
@@ -37,7 +37,7 @@ module Byebug
     def test_run_with_remote_option_with_host_and_port_specification
       ARGV.replace(%w(--remote myhost:9999))
       Byebug.expects(:start_client)
-      Byebug::Runner.new.run
+      @runner.run
 
       check_output_includes(/Connecting to byebug server myhost:9999/)
     end
@@ -45,7 +45,7 @@ module Byebug
     def test_run_without_a_script_to_debug
       ARGV.replace([])
 
-      assert_raises(SystemExit) { Byebug::Runner.new.run }
+      assert_raises(SystemExit) { @runner.run }
 
       check_output_includes(/You must specify a program to debug.../)
     end
@@ -60,13 +60,13 @@ module Byebug
       ARGV.replace(%w(my_script))
       expect_it_debugs_script
 
-      Byebug::Runner.new.run
+      @runner.run
     end
 
     def test_debugged_program_from_argv
       ARGV.replace(%w(my_script -opt value))
 
-      assert_match 'my_script', Byebug::Runner.new.debugged_program_from_argv
+      assert_match 'my_script', @runner.debugged_program_from_argv
     end
 
     def test_run_with_no_rc_option
@@ -75,14 +75,14 @@ module Byebug
       Byebug::Runner.any_instance.expects(:debug_program)
       Byebug.expects(:run_init_script).never
 
-      Byebug::Runner.new.run
+      @runner.run
     end
 
     def test_run_with_post_mortem_mode_flag
       ARGV.replace(%w(-m my_script))
       expect_it_debugs_script
+      @runner.run
 
-      Byebug::Runner.new.run
       assert_equal true, Byebug.post_mortem?
       Byebug::Setting[:post_mortem] = false
     end
@@ -90,8 +90,8 @@ module Byebug
     def test_run_with_linetracing_flag
       ARGV.replace(%w(-t my_script))
       expect_it_debugs_script
+      @runner.run
 
-      Byebug::Runner.new.run
       assert_equal true, Byebug.tracing?
       Byebug::Setting[:linetrace] = false
     end
@@ -99,16 +99,16 @@ module Byebug
     def test_run_with_no_quit_flag
       skip 'for now'
       ARGV.replace(%w(--no-quit my_script))
+      @runner.run
 
-      Byebug::Runner.new.run
       check_output_includes('(byebug:ctrl)')
     end
 
     def test_run_with_require_flag
       ARGV.replace(%w(-r abbrev my_script))
       expect_it_debugs_script
+      @runner.run
 
-      Byebug::Runner.new.run
       hsh = { 'can' => 'can', 'cat' => 'cat' }
       assert_equal hsh, %w(can cat).abbrev
     end
@@ -116,16 +116,16 @@ module Byebug
     def test_run_with_include_flag
       ARGV.replace(%w(-I custom_dir my_script))
       expect_it_debugs_script
+      @runner.run
 
-      Byebug::Runner.new.run
       assert_includes $LOAD_PATH, 'custom_dir'
     end
 
     def test_run_with_debug_flag
       ARGV.replace(%w(-d my_script))
       expect_it_debugs_script
+      @runner.run
 
-      Byebug::Runner.new.run
       assert_equal $DEBUG, true
       $DEBUG = false
     end
