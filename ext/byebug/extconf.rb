ec728f2b69a67b02dff6a5116eadd46e694f201a
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-10-01 07:54:34 +0200

    Restore line accidentally removed

--- ext/byebug/extconf.rb
+++ ext/byebug/extconf.rb
@@ -10,4 +10,5 @@ RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 RbConfig::MAKEFILE_CONFIG['CFLAGS'] << ' -Wall -Werror -Wno-unused-parameter'
 RbConfig::MAKEFILE_CONFIG['CFLAGS'] << ' -gdwarf-2 -g3 -O0' if ENV['debug']
 
+dir_config('ruby')
 create_makefile('byebug/byebug')
