4fdd2abe06bfa65fa58c6e4201bf91d5898def3c
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-10-03 12:08:13 +0200

    Don't even define thread tests for now
    Merely skipping them was causing some random failures (seed 7872)

--- test/commands/thread_test.rb
+++ test/commands/thread_test.rb
@@ -25,7 +25,7 @@ module Byebug
     def kill
       @t2.kill
     end
-  end
+  end unless true
 
   class ThreadTestCase < TestCase
     def setup
@@ -53,7 +53,6 @@ module Byebug
     end
 
     def test_thread_list_marks_current_thread_with_a_plus_sign
-      skip 'for now'
       thnum = nil
       enter 'break 8', 'cont', 'thread list', release
       debug_proc(@example) { thnum = first_thnum }
@@ -61,7 +60,6 @@ module Byebug
     end
 
     def test_thread_list_works_with_shortcut
-      skip 'for now'
       thnum = nil
       enter 'break 8', 'cont', 'th list', release
       debug_proc(@example) { thnum = first_thnum }
@@ -69,7 +67,6 @@ module Byebug
     end
 
     def test_thread_list_show_all_available_threads
-      skip 'for now'
       enter 'break 21', 'cont', 'thread list', release
       debug_proc(@example)
       check_output_includes(/(\+)?\d+ #<Thread:\S+ (sleep|run)>/,
@@ -78,7 +75,6 @@ module Byebug
     end
 
     def test_thread_stop_marks_thread_as_suspended
-      skip 'for now'
       thnum = nil
       enter 'break 21', 'cont', -> { "thread stop #{last_thnum}" }, release
       debug_proc(@example) { thnum = last_thnum }
@@ -86,7 +82,6 @@ module Byebug
     end
 
     def test_thread_stop_actually_suspends_thread_execution
-      skip 'for now'
       enter 'break 21', 'cont', 'trace on',
             -> { "thread stop #{last_thnum}" }, release
       debug_proc(@example)
@@ -95,21 +90,18 @@ module Byebug
     end
 
     def test_thread_stop_shows_error_when_thread_number_not_specified
-      skip 'for now'
       enter 'break 8', 'cont', 'thread stop', release
       debug_proc(@example)
       check_error_includes '"thread stop" needs a thread number'
     end
 
     def test_thread_stop_shows_error_when_trying_to_stop_current_thread
-      skip 'for now'
       enter 'break 8', 'cont', -> { "thread stop #{first_thnum}" }, release
       debug_proc(@example)
       check_error_includes "It's the current thread"
     end
 
     def test_thread_resume_removes_threads_from_the_suspended_state
-      skip 'for now'
       thnum = nil
       enter 'break 21', 'cont',
             -> { thnum = last_thnum ; "thread stop #{thnum}" },
@@ -121,44 +113,38 @@ module Byebug
     end
 
     def test_thread_resume_shows_error_if_thread_number_not_specified
-      skip 'for now'
       enter 'break 8', 'cont', 'thread resume', release
       debug_proc(@example)
       check_error_includes '"thread resume" needs a thread number'
     end
 
     def test_thread_resume_shows_error_when_trying_to_resume_current_thread
-      skip 'for now'
       enter 'break 8', 'cont', -> { "thread resume #{first_thnum}" }, release
       debug_proc(@example)
       check_error_includes "It's the current thread"
     end
 
     def test_thread_resume_shows_error_if_thread_is_already_running
-      skip 'for now'
       enter 'break 21', 'cont', -> { "thread resume #{last_thnum}" }, release
       debug_proc(@example)
       check_error_includes 'Already running'
     end
 
     def test_thread_switch_changes_execution_to_another_thread
-      skip 'for now'
       enter 'break 21', 'cont', -> { "thread switch #{last_thnum}" }, release
       debug_proc(@example) { assert_equal state.line, 16 }
     end
 
     def test_thread_switch_shows_error_if_thread_number_not_specified
-      skip 'for now'
       enter 'break 8', 'cont', 'thread switch', release
       debug_proc(@example)
       check_error_includes '"thread switch" needs a thread number'
     end
 
     def test_thread_switch_shows_error_when_trying_to_switch_current_thread
-      skip 'for now'
       enter 'break 8', 'cont', -> { "thread switch #{first_thnum}" }, release
       debug_proc(@example)
       check_error_includes "It's the current thread"
     end
-  end
+  end unless true
 end
