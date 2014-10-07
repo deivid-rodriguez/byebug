e44e2fad6f1aff351cced9dfbc6452e9925b6369
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-10-07 17:02:53 +0200

    Standarize a couple of comments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,7 +14,9 @@ require 'byebug'
 require_relative 'support/utils'
 require_relative 'support/test_case'
 
+#
 # Load the test files from the command line.
+#
 argv = ARGV.select do |argument|
   case argument
   when /^-/ then
@@ -30,5 +32,7 @@ argv = ARGV.select do |argument|
   end
 end
 
+#
 # Run the tests
+#
 Minitest.run argv
