35a0730aac6fc67892f58a7b35ff4691f6d3a0f3
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-10-08 10:16:01 +0200

    Refactor variable tests
    - Did TODO's.
    - Migrated tests to use new debug_code method.

--- test/commands/variables_test.rb
+++ test/commands/variables_test.rb
@@ -1,104 +1,107 @@
 module Byebug
-  class VariablesExample
-    SOMECONST = 'foo' unless defined?(SOMECONST)
-
-    def initialize
-      @inst_a = 1
-      @inst_b = 2
-      @inst_c = '1' * 40
-      @inst_d = BasicObject.new
-    end
-
-    def run
-      a = 4
-      b = [1, 2, 3].map do |i|
-        a * i
-      end
-      b
-    end
-  end
-
+  #
+  # Tests variable evaluation.
+  #
   class VariablesTestCase < TestCase
-    def setup
-      @example = lambda do
-        byebug
-
-        v = VariablesExample.new
-        v.run
-      end
-
-      super
+    def program
+      strip_line_numbers <<-EOC
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test variable evaluation.
+         4:    #
+         5:    class TestExample
+         6:      SOMECONST = 'foo' unless defined?(SOMECONST)
+         7:      @@class_variable = 'bar'
+         8:
+         9:      def initialize
+        10:        @instance_variable = '1' * 20
+        11:        byebug
+        12:        @weird_instance_variable = BasicObject.new
+        13:      end
+        14:
+        15:      def run(level)
+        16:        [1, 2, 3].map do |i|
+        17:          level * i
+        18:        end
+        19:      end
+        20:    end
+        21:
+        22:    v = TestExample.new
+        23:    v.run(2)
+        24:  end
+      EOC
     end
 
-    # TODO: we check a class minitest variable... brittle...
     ['var class', 'v cl'].each do |cmd_alias|
       define_method(:"test_#{cmd_alias}_shows_class_variables") do
         enter cmd_alias
-        debug_proc(@example)
-        check_output_includes(/@@runnables/)
+        debug_code(program)
+        check_output_includes '@@class_variable = "bar"'
       end
     end
 
     ['var const', 'v co'].each do |cmd_alias|
       define_method(:"test_#{cmd_alias}_shows_constants_in_class_or_module") do
-        enter "#{cmd_alias} VariablesExample"
-        debug_proc(@example)
+        enter "#{cmd_alias} Byebug::TestExample"
+        debug_code(program)
         check_output_includes 'SOMECONST => "foo"'
       end
     end
 
     def test_var_const_shows_error_if_given_object_is_not_a_class_or_module
       enter 'var const v'
-      debug_proc(@example)
+      debug_code(program)
       check_output_includes 'Should be Class/Module: v'
     end
 
     ['var global', 'v g'].each do |cmd_alias|
       define_method(:"test_#{cmd_alias}_shows_global_variables") do
         enter cmd_alias
-        debug_proc(@example)
-        check_output_includes '$VERBOSE = true'
+        debug_code(program)
+        check_output_includes '$ERROR_INFO = nil'
       end
     end
 
     ['var instance', 'v ins'].each do |cmd_alias|
       define_method(:"test_#{cmd_alias}_shows_instance_vars_of_an_object") do
-        enter 'break 27', 'cont', "#{cmd_alias} v"
-        debug_proc(@example)
-        check_output_includes '@inst_a = 1', '@inst_b = 2'
+        enter 'break 23', 'cont', "#{cmd_alias} v"
+        debug_code(program)
+        check_output_includes '@instance_variable = "11111111111111111111"'
       end
     end
 
     def test_var_instance_shows_instance_variables_of_self_if_no_object_given
-      enter 'break 9', 'cont', 'var instance'
-      debug_proc(@example)
-      check_output_includes '@inst_a = 1', '@inst_b = 2'
+      enter 'var instance'
+      debug_code(program)
+      check_output_includes '@instance_variable = "11111111111111111111"'
     end
 
     def test_var_instance_cuts_long_variable_values_according_to_width_setting
-      enter 'break 27', 'cont', 'set width 45', 'var instance v'
-      debug_proc(@example)
-      check_output_includes '@inst_c = "1111111111111111111111111111111...'
+      enter 'set width 40', 'var instance'
+      debug_code(program)
+      check_output_includes '@instance_variable = "111111111111111...'
     end
 
     def test_v_ins_shows_error_if_value_does_not_have_to_s_or_inspect_methods
-      enter 'break 27', 'cont', 'v ins v'
-      debug_proc(@example)
-      check_output_includes '@inst_d = *Error in evaluation*'
+      enter 'break 23', 'cont', 'v ins v'
+      debug_code(program)
+      check_output_includes '@weird_instance_variable = *Error in evaluation*'
     end
 
     def test_var_local_shows_local_variables
-      enter 'break 15', 'cont', 'var local'
-      debug_proc(@example)
-      check_output_includes 'a => 4', 'b => nil', 'i => 1'
+      enter 'break 17', 'cont', 'var local'
+      debug_code(program)
+      check_output_includes 'level => 2', 'i => 1'
     end
 
-    # TODO: class variables not currently checked
     ['var all', 'v a'].each do |cmd_alias|
       define_method(:"test_#{cmd_alias}_shows_all_variables") do
-        enter 'break 15', 'cont', cmd_alias
-        debug_proc(@example)
-        check_output_includes '$VERBOSE = true', '@inst_a = 1', 'a => 4'
+        enter 'break 17', 'cont', cmd_alias
+        debug_code(program)
+        check_output_includes '@@class_variable = "bar"',
+                              '$ERROR_INFO = nil',
+                              '@instance_variable = "11111111111111111111"',
+                              'level => 2'
       end
     end
   end
