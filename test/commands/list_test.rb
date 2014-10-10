a23781cbfa1946e202ea56bad4269e1c36b99666
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2014-10-10 13:51:15 +0200

    Rewrite list tests to use debug_code method

--- test/commands/list_test.rb
+++ test/commands/list_test.rb
@@ -1,176 +1,155 @@
 module Byebug
+  #
+  # Tests for listing source files.
+  #
   class ListTestCase < TestCase
-    def setup
-      @example = lambda do
-        byebug
-        a = '6'
-        a += '7'
-        a += '8'
-        a += '9'
-        a += '10'
-        a += '11'
-        a += '12'
-        a += '13'
-        a += '14'
-        a += '15'
-        a += '16'
-        a += '17'
-        a += '18'
-        a += '19'
-        a += '20'
-        a += '21'
-        a += '22'
-        a += '23'
-        a += '24'
-        a += '25'
-        a + '%26'
-      end
-
-      super
+    def program
+      strip_line_numbers <<-EOC
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test breakpoints
+         4:    #
+         5:    class TestExample
+         6:      def build_percentage_string
+         7:        '%1'
+         8:      end
+         9:    end
+        10:
+        11:    byebug
+        12:
+        13:    str = TestExample.new.build_percentage_string
+        14:
+        15:    str
+        16:  end
+      EOC
     end
 
     def test_lists_source_code_lines
-      Setting[:listsize] = 10
-      enter 'list'
-      debug_proc(@example)
-      check_output_includes "[1, 10] in #{__FILE__}"
+      enter 'set listsize 3', 'list'
+      debug_code(program)
+      check_output_includes "[12, 14] in #{example_path}"
     end
 
     def test_listsize_is_not_set_if_parameter_is_not_an_integer
-      enter 'set listsize 15.0', 'list'
-      debug_proc(@example)
-      check_output_doesnt_include "[1, 15] in #{__FILE__}"
+      enter 'set listsize 5.0', 'list'
+      debug_code(program)
+      check_output_doesnt_include "[12, 14] in #{example_path}"
     end
 
-    def test_moves_range_up_when_it_goes_before_beginning_of_file
-      Setting[:listsize] = 12
-      enter 'list'
-      debug_proc(@example)
-      check_output_includes "[1, 12] in #{__FILE__}"
+    def test_does_not_list_before_the_beginning_of_file
+      enter 'cont 7', 'set listsize 15', 'list'
+      debug_code(program)
+      check_output_includes "[1, 15] in #{example_path}"
     end
 
     def test_does_not_list_after_the_end_of_file
-      n_lines = `wc -l #{__FILE__}`.split.first.to_i
-      enter 'break 18', 'cont', "list #{n_lines - 3}-#{n_lines + 6}"
-      debug_proc(@example)
-      check_output_includes "[#{n_lines - 3}, #{n_lines}] in #{__FILE__}"
+      enter 'set listsize 13', 'list'
+      debug_code(program)
+      check_output_includes "[4, 16] in #{example_path}"
     end
 
     def test_lists_the_whole_file_if_number_of_lines_is_smaller_than_listsize
-      Setting[:listsize] = 1000
-      n_lines = `wc -l #{__FILE__}`.split.first.to_i
-      enter 'list'
-      debug_proc(@example)
-      check_output_includes "[1, #{n_lines}] in #{__FILE__}"
-    end
-
-    def test_lists_surrounding_lines_after_the_first_call_to_list
-      enter 'break 8', 'cont', 'list'
-      debug_proc(@example)
-      check_output_includes "[3, 12] in #{__FILE__}"
+      enter 'set listsize 17', 'list'
+      debug_code(program)
+      check_output_includes "[1, 16] in #{example_path}"
     end
 
     def test_lists_forwards_after_the_second_call_to_list
-      enter 'break 8', 'cont', 'list', 'list'
-      debug_proc(@example)
-      check_output_includes "[13, 22] in #{__FILE__}"
+      enter 'set listsize 3', 'cont 7', 'list', 'list'
+      debug_code(program)
+      check_output_includes "[9, 11] in #{example_path}"
     end
 
     def test_lists_surrounding_lines_after_the_first_call_to_list_minus
-      enter 'break 18', 'cont', 'list -'
-      debug_proc(@example)
-      check_output_includes "[13, 22] in #{__FILE__}"
+      enter 'set listsize 3', 'list-'
+      debug_code(program)
+      check_output_includes "[12, 14] in #{example_path}"
     end
 
     def test_lists_backwards_after_the_second_call_to_list_minus
-      enter 'break 18', 'cont', 'list -', 'list -'
-      debug_proc(@example)
-      check_output_includes "[3, 12] in #{__FILE__}"
+      enter 'set listsize 3', 'list-', 'list-'
+      debug_code(program)
+      check_output_includes "[9, 11] in #{example_path}"
     end
 
     def test_lists_backwards_from_end_of_file
-      n_lines = `wc -l #{__FILE__}`.split.first.to_i
-      enter 'break 18', 'cont', "list #{n_lines - 9}-#{n_lines}", 'list -'
-      debug_proc(@example)
-      check_output_includes "[#{n_lines - 19}, #{n_lines - 10}] in #{__FILE__}"
+      enter 'set listsize 3', 'list 14-16', 'list -'
+      debug_code(program)
+      check_output_includes "[11, 13] in #{example_path}"
     end
 
     def test_lists_surrounding_lines_when_list_equals_is_called
-      enter 'break 8', 'cont', 'list ='
-      debug_proc(@example)
-      check_output_includes "[3, 12] in #{__FILE__}"
+      enter 'set listsize 3', 'list ='
+      debug_code(program)
+      check_output_includes "[12, 14] in #{example_path}"
     end
 
     def test_lists_specific_range_when_requested_in_hyphen_format
-      enter 'list 7-9'
-      debug_proc(@example)
-      check_output_includes "[7, 9] in #{__FILE__}"
+      enter 'list 6-8'
+      debug_code(program)
+      check_output_includes "[6, 8] in #{example_path}"
     end
 
     def test_lists_specific_range_when_requested_in_comma_format
-      enter 'list 7,9'
-      debug_proc(@example)
-      check_output_includes "[7, 9] in #{__FILE__}"
+      enter 'list 6,8'
+      debug_code(program)
+      check_output_includes "[6, 8] in #{example_path}"
     end
 
     def test_lists_nothing_if_unexistent_range_is_specified
-      enter 'list 500,505'
-      debug_proc(@example)
+      enter 'list 20,25'
+      debug_code(program)
       check_error_includes 'Invalid line range'
-      check_output_doesnt_include "[500, 505] in #{__FILE__}"
+      check_output_doesnt_include "[20, 25] in #{example_path}"
     end
 
     def test_lists_nothing_if_invalid_range_is_specified
       enter 'list 5,4'
-      debug_proc(@example)
+      debug_code(program)
       check_error_includes 'Invalid line range'
-      check_output_doesnt_include "[5, 4] in #{__FILE__}"
+      check_output_doesnt_include "[5, 4] in #{example_path}"
     end
 
     def test_list_proper_lines_when_range_around_specific_line_with_hyphen
-      enter 'list 17-'
-      debug_proc(@example)
-      check_output_includes "[12, 21] in #{__FILE__}"
+      enter 'set listsize 3', 'list 4-'
+      debug_code(program)
+      check_output_includes "[3, 5] in #{example_path}"
     end
 
     def test_list_proper_lines_when_range_around_specific_line_with_comma
-      enter 'list 17,'
-      debug_proc(@example)
-      check_output_includes "[12, 21] in #{__FILE__}"
+      enter 'set listsize 3', 'list 4,'
+      debug_code(program)
+      check_output_includes "[3, 5] in #{example_path}"
     end
 
     def test_shows_an_error_when_the_file_to_list_does_not_exist
       enter -> { state.file = 'blabla'; 'list 7-7' }
-      debug_proc(@example)
+      debug_code(program)
       check_error_includes 'No sourcefile available for blabla'
     end
 
     def test_correctly_print_lines_containing_the_percentage_symbol
-      enter 'list 26'
-      debug_proc(@example)
-      check_output_includes "26:         a + '%26'"
+      enter 'list 7'
+      debug_code(program)
+      check_output_includes(/7:\s+'%1'/)
     end
 
     def test_lists_file_changes_by_default
       enter \
-        -> { cmd_after_replace(__FILE__, 7, "        a += '-1'", 'list 7-7') }
+        -> { cmd_after_replace(example_path, 7, "      '%2'", 'list 7-7') }
 
-      debug_proc(@example)
-      check_output_includes(/7:\s+a \+= '-1'/)
-    ensure
-      change_line(__FILE__, 7, "        a += '7'")
+      debug_code(program)
+      check_output_includes(/7:\s+'%2'/)
     end
 
     def test_does_not_list_file_changes_with_autoreload_disabled
       enter \
-        'set noautoreload', 'list', # to force a first read of the file
-        -> { cmd_after_replace(__FILE__, 7, "        a += '-1'", 'list 7-7') },
-        'set autoreload'
+        'list',
+        'set noautoreload',
+        -> { cmd_after_replace(example_path, 7, "      '%2'", 'list 7-7') }
 
-      debug_proc(@example)
-      check_output_doesnt_include(/7:\s+a \+= '-1'/)
-    ensure
-      change_line(__FILE__, 7, "        a += '7'")
+      debug_code(program)
+      check_output_doesnt_include(/7:\s+'%2'/)
     end
   end
 end
