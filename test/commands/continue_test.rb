ff134a0a323b2e84d7d6bd8951729c279ef02d79
David Rodríguez <deivid.rodriguez@gmail.com>
2014-10-14 15:26:58 +0200

    Migrate continue test to use debug_code method

--- test/commands/continue_test.rb
+++ test/commands/continue_test.rb
@@ -1,46 +1,52 @@
 module Byebug
-  class ContinueExample
-    def self.a(num)
-      num + 4
-    end
-  end
-
+  #
+  # Tests for continue command
+  #
   class ContinueTestCase < TestCase
-    def setup
-      @example = lambda do
-        byebug
-
-        b = 5
-        c = b + 5
-        ContinueExample.a(c)
-      end
-
-      super
+    def program
+      strip_line_numbers <<-EOC
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test continue command.
+         4:    #
+         5:    class TestExample
+         6:      def add_four(num)
+         7:        num + 4
+         8:      end
+         9:    end
+        10:
+        11:    byebug
+        12:
+        13:    b = 5
+        14:    c = b + 5
+        15:    TestExample.new.add_four(c)
+        16:  end
+      EOC
     end
 
     def test_continues_up_to_breakpoint_if_no_line_specified
       enter 'break 14', 'continue'
-      debug_proc(@example) { assert_equal 14, state.line }
+      debug_code(program) { assert_equal 14, state.line }
     end
 
     def test_works_in_abbreviated_mode_too
       enter 'break 14', 'cont'
-      debug_proc(@example) { assert_equal 14, state.line }
+      debug_code(program) { assert_equal 14, state.line }
     end
 
     def test_continues_up_to_the_specified_line
       enter 'cont 14'
-      debug_proc(@example) { assert_equal 14, state.line }
+      debug_code(program) { assert_equal 14, state.line }
     end
 
     def test_ignores_the_command_if_specified_line_is_not_valid
       enter 'cont 100'
-      debug_proc(@example) { assert_equal 13, state.line }
+      debug_code(program) { assert_equal 13, state.line }
     end
 
     def test_shows_error_if_specified_line_is_not_valid
       enter 'cont 100'
-      debug_proc(@example)
+      debug_code(program)
       check_error_includes 'Line 100 is not a valid stopping point in file'
     end
   end
