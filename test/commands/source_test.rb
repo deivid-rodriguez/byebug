9a0b352d81fb27b14f5c3cff0b0b17e12df52a3f
David Rodríguez <deivid.rodriguez@gmail.com>
2014-12-08 16:40:32 +0100

    Whitespace
    The idea is to visually separate actions from assertions.

--- test/commands/source_test.rb
+++ test/commands/source_test.rb
@@ -33,6 +33,7 @@ module Byebug
     %w(source so).each do |cmd_alias|
       define_method(:"test_#{cmd_alias}_runs_byebug_commands_from_file") do
         enter "#{cmd_alias} source_example.txt"
+
         debug_code(program) do
           assert_equal 4, Breakpoint.first.pos
           assert_equal 5, Breakpoint.last.pos
@@ -43,12 +44,14 @@ module Byebug
       define_method(:"test_#{cmd_alias}_shows_an_error_if_file_not_found") do
         enter "#{cmd_alias} blabla"
         debug_code(program)
+
         check_error_includes(/File ".*blabla" not found/)
       end
 
       define_method(:"test_#{cmd_alias}_without_arguments_shows_help") do
         enter 'source'
         debug_code(program)
+
         check_output_includes(
           /Executes file <file> containing byebug commands./)
       end
