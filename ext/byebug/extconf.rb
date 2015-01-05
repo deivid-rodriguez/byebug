8e23c185a4e771b9bf55a5e28327decd55da2e04
David Rodríguez <deivid.rodriguez@gmail.com>
2015-01-05 13:33:04 +0100

    Ignore unknown warnings only in clang
    That flag is not implemented in gcc.

--- ext/byebug/extconf.rb
+++ ext/byebug/extconf.rb
@@ -9,8 +9,12 @@ makefile_config = RbConfig::MAKEFILE_CONFIG
 
 makefile_config['CC'] = ENV['CC'] if ENV['CC']
 
-makefile_config['CFLAGS'] << ' -Wall -Werror -Wno-unknown-warning-option'
+makefile_config['CFLAGS'] << ' -Wall -Werror'
 makefile_config['CFLAGS'] << ' -gdwarf-2 -g3 -O0' if ENV['debug']
+
+if makefile_config['CC'] =~ /clang/
+  makefile_config['CFLAGS'] << ' -Wno-unknown-warning-option'
+end
 
 dir_config('ruby')
 with_cflags(makefile_config['CFLAGS']) { create_makefile('byebug/byebug') }
