deivid-rodriguez/byebug

commit 663d95db02c3b7c864ad7c8089026a0c8839a531

Fix issue with class/b_call end/b_return events
This kind of events had the same callbacks as the "call" event, where a
method_id is assumed the be included in the TracePoint information.
However, this kind of events don't contain that kind of information.
This was the reason for: https://bugs.ruby-lang.org/issues/9937. The fix
consists on associating to those events the simpler callback for the c_call
event that only updates calculated callstack size information

ext/byebug/byebug.c
@@ -288,7 +288,7 @@ line_event(VALUE trace_point, void *data)
 static void
 call_event(VALUE trace_point, void *data)
 {
-  VALUE breakpoint, klass, msym, mid, binding, self, file, line;
+  VALUE breakpoint, klass, mid, binding, self, file, line;
 
   EVENT_SETUP;
 
@@ -301,9 +301,8 @@ call_event(VALUE trace_point, void *data)
 
   breakpoint = Qnil;
   klass = rb_tracearg_defined_class(trace_arg);
-  msym = rb_tracearg_method_id(trace_arg);
-  mid = NIL_P(msym) ? Qnil : SYM2ID(msym);
   binding = rb_tracearg_binding(trace_arg);
+  mid = SYM2ID(rb_tracearg_method_id(trace_arg));
   self = rb_tracearg_self(trace_arg);
   file = rb_tracearg_path(trace_arg);
   line = rb_tracearg_lineno(trace_arg);
@@ -349,7 +348,7 @@ return_event(VALUE trace_point, void *data)
 }
 
 static void
-c_call_event(VALUE trace_point, void *data)
+msc_call_event(VALUE trace_point, void *data)
 {
   EVENT_SETUP;
 
@@ -361,7 +360,7 @@ c_call_event(VALUE trace_point, void *data)
 }
 
 static void
-c_return_event(VALUE trace_point, void *data)
+msc_return_event(VALUE trace_point, void *data)
 {
   EVENT_SETUP;
 
@@ -455,27 +454,29 @@ register_tracepoints(VALUE self)
   if (NIL_P(traces))
   {
     int line_msk = RUBY_EVENT_LINE;
-    int call_msk = RUBY_EVENT_CALL | RUBY_EVENT_B_CALL | RUBY_EVENT_CLASS;
-    int return_msk = RUBY_EVENT_RETURN | RUBY_EVENT_B_RETURN | RUBY_EVENT_END;
-    int c_call_msk = RUBY_EVENT_C_CALL;
-    int c_ret_msk = RUBY_EVENT_C_RETURN;
+    int call_msk = RUBY_EVENT_CALL;
+    int ret_msk = RUBY_EVENT_RETURN;
+    int msc_call_msk =
+      RUBY_EVENT_C_CALL | RUBY_EVENT_B_CALL | RUBY_EVENT_CLASS;
+    int msc_ret_msk =
+      RUBY_EVENT_C_RETURN | RUBY_EVENT_B_RETURN | RUBY_EVENT_END;
     int raise_msk = RUBY_EVENT_RAISE;
     int thread_msk = RUBY_EVENT_THREAD_BEGIN | RUBY_EVENT_THREAD_END;
 
     VALUE tpLine = rb_tracepoint_new(Qnil, line_msk, line_event, 0);
     VALUE tpCall = rb_tracepoint_new(Qnil, call_msk, call_event, 0);
-    VALUE tpReturn = rb_tracepoint_new(Qnil, return_msk, return_event, 0);
-    VALUE tpCCall = rb_tracepoint_new(Qnil, c_call_msk, c_call_event, 0);
-    VALUE tpCReturn = rb_tracepoint_new(Qnil, c_ret_msk, c_return_event, 0);
+    VALUE tpRet = rb_tracepoint_new(Qnil, ret_msk, return_event, 0);
+    VALUE tpMscCall = rb_tracepoint_new(Qnil, msc_call_msk, msc_call_event, 0);
+    VALUE tpMscRet = rb_tracepoint_new(Qnil, msc_ret_msk, msc_return_event, 0);
     VALUE tpRaise = rb_tracepoint_new(Qnil, raise_msk, raise_event, 0);
     VALUE tpThread = rb_tracepoint_new(Qnil, thread_msk, thread_event, 0);
 
     traces = rb_ary_new();
     rb_ary_push(traces, tpLine);
     rb_ary_push(traces, tpCall);
-    rb_ary_push(traces, tpReturn);
-    rb_ary_push(traces, tpCCall);
-    rb_ary_push(traces, tpCReturn);
+    rb_ary_push(traces, tpRet);
+    rb_ary_push(traces, tpMscCall);
+    rb_ary_push(traces, tpMscRet);
     rb_ary_push(traces, tpRaise);
     rb_ary_push(traces, tpThread);
 

commit 8229a251b04c5171118e6a32bcbd08bc43668488

Really fix https://bugs.ruby-lang.org/issues/9937
The "fix" introduced in 663d95db was wrong and caused some regressions.
Really fix it and add a couple of regression tests.

ext/byebug/byebug.c
@@ -78,14 +78,6 @@ check_started()
   }
 }
 
-const char *
-safe_sym_to_str(VALUE sym)
-{
-  VALUE id = NIL_P(sym) ? Qnil : SYM2ID(sym);
-
-  return NIL_P(id) ? "" : rb_id2name(id);
-}
-
 static void
 trace_print(rb_trace_arg_t * trace_arg, debug_context_t * dc,
             const char *file_filter, const char *debug_msg)
@@ -100,7 +92,8 @@ trace_print(rb_trace_arg_t * trace_arg, debug_context_t * dc,
 
   int line = NUM2INT(rb_tracearg_lineno(trace_arg));
 
-  const char *mid = safe_sym_to_str(rb_tracearg_method_id(trace_arg));
+  VALUE rb_mid = rb_tracearg_method_id(trace_arg);
+  const char *mid = NIL_P(rb_mid) ? "(top level)" : rb_id2name(SYM2ID(rb_mid));
 
   VALUE rb_cl = rb_tracearg_defined_class(trace_arg);
   VALUE rb_cl_name = NIL_P(rb_cl) ? rb_cl : rb_mod_name(rb_cl);
@@ -331,7 +324,7 @@ line_event(VALUE trace_point, void *data)
 static void
 call_event(VALUE trace_point, void *data)
 {
-  VALUE breakpoint, klass, mid, binding, self, file, line;
+  VALUE breakpoint, klass, msym, mid, binding, self, file, line;
 
   EVENT_SETUP;
 
@@ -344,10 +337,18 @@ call_event(VALUE trace_point, void *data)
 
   dc->steps_out = dc->steps_out <= 0 ? -1 : dc->steps_out + 1;
 
-  breakpoint = Qnil;
+  /* nil method_id means we are at top level so there can't be a method
+   * breakpoint here. Just leave then. */
+  msym = rb_tracearg_method_id(trace_arg);
+  if (NIL_P(msym))
+  {
+    EVENT_TEARDOWN;
+    return;
+  }
+
+  mid = SYM2ID(msym);
   klass = rb_tracearg_defined_class(trace_arg);
   binding = rb_tracearg_binding(trace_arg);
-  mid = SYM2ID(rb_tracearg_method_id(trace_arg));
   self = rb_tracearg_self(trace_arg);
   file = rb_tracearg_path(trace_arg);
   line = rb_tracearg_lineno(trace_arg);
@@ -391,7 +392,7 @@ return_event(VALUE trace_point, void *data)
 }
 
 static void
-msc_call_event(VALUE trace_point, void *data)
+c_call_event(VALUE trace_point, void *data)
 {
   EVENT_SETUP;
 
@@ -401,7 +402,7 @@ msc_call_event(VALUE trace_point, void *data)
 }
 
 static void
-msc_return_event(VALUE trace_point, void *data)
+c_return_event(VALUE trace_point, void *data)
 {
   EVENT_SETUP;
 
@@ -499,19 +500,17 @@ register_tracepoints(VALUE self)
   if (NIL_P(traces))
   {
     int line_msk = RUBY_EVENT_LINE;
-    int call_msk = RUBY_EVENT_CALL;
-    int ret_msk = RUBY_EVENT_RETURN;
-    int msc_call_msk =
-      RUBY_EVENT_C_CALL | RUBY_EVENT_B_CALL | RUBY_EVENT_CLASS;
-    int msc_ret_msk =
-      RUBY_EVENT_C_RETURN | RUBY_EVENT_B_RETURN | RUBY_EVENT_END;
+    int call_msk = RUBY_EVENT_CALL | RUBY_EVENT_B_CALL | RUBY_EVENT_CLASS;
+    int return_msk = RUBY_EVENT_RETURN | RUBY_EVENT_B_RETURN | RUBY_EVENT_END;
+    int c_call_msk = RUBY_EVENT_C_CALL;
+    int c_ret_msk = RUBY_EVENT_C_RETURN;
     int raise_msk = RUBY_EVENT_RAISE;
 
     VALUE tpLine = rb_tracepoint_new(Qnil, line_msk, line_event, 0);
     VALUE tpCall = rb_tracepoint_new(Qnil, call_msk, call_event, 0);
-    VALUE tpRet = rb_tracepoint_new(Qnil, ret_msk, return_event, 0);
-    VALUE tpMscCall = rb_tracepoint_new(Qnil, msc_call_msk, msc_call_event, 0);
-    VALUE tpMscRet = rb_tracepoint_new(Qnil, msc_ret_msk, msc_return_event, 0);
+    VALUE tpReturn = rb_tracepoint_new(Qnil, return_msk, return_event, 0);
+    VALUE tpCCall = rb_tracepoint_new(Qnil, c_call_msk, c_call_event, 0);
+    VALUE tpCReturn = rb_tracepoint_new(Qnil, c_ret_msk, c_return_event, 0);
     VALUE tpRaise = rb_tracepoint_new(Qnil, raise_msk, raise_event, 0);
 
     VALUE tpThreadBegin = rb_tracepoint_new(Qnil,
@@ -527,9 +526,9 @@ register_tracepoints(VALUE self)
     traces = rb_ary_new();
     rb_ary_push(traces, tpLine);
     rb_ary_push(traces, tpCall);
-    rb_ary_push(traces, tpRet);
-    rb_ary_push(traces, tpMscCall);
-    rb_ary_push(traces, tpMscRet);
+    rb_ary_push(traces, tpReturn);
+    rb_ary_push(traces, tpCCall);
+    rb_ary_push(traces, tpCReturn);
     rb_ary_push(traces, tpRaise);
     rb_ary_push(traces, tpThreadBegin);
     rb_ary_push(traces, tpThreadEnd);

test/commands/break_test.rb
@@ -327,14 +327,14 @@ module Byebug
   end
 
   #
-  # Tests entering byebug at the end of a method.
+  # Tests using the byebug keyword at special places
   #
-  class BreakDeepTestCase < TestCase
-    def program
-      strip_line_numbers <<-EOC
+  class BreakWithByebugKeyword < TestCase
+    def test_stops_at_method_end_when_last_instruction_of_method
+      program = strip_line_numbers <<-EOC
          1:  module Byebug
          2:    #
-         3:    # Toy class to test breakpoints
+         3:    # Toy class to test byebug at the end of a method
          4:    #
          5:    class #{example_class}
          6:      def a
@@ -345,10 +345,50 @@ module Byebug
         11:    end
         12:  end
       EOC
+
+      debug_code(program) { assert_equal 8, state.line }
     end
 
-    def test_breaking_w_byebug_keywork_stops_at_frame_end_when_last_instruction
-      debug_code(program) { assert_equal 8, state.line }
+    def test_stops_at_block_end_when_last_instruction_of_block
+      program = strip_line_numbers <<-EOC
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test byebug at the end of a block
+         4:    #
+         5:    class #{example_class}
+         6:      def method_that_yields(b)
+         7:        yield(b)
+         8:        0
+         9:      end
+        10:
+        11:      new.method_that_yields(0) do |n|
+        12:        sleep n
+        13:        byebug
+        14:      end
+        15:    end
+        16:  end
+      EOC
+
+      debug_code(program) { assert_equal 14, state.line }
+    end
+
+    def test_stops_at_class_end_when_last_instruction_of_class
+      program = strip_line_numbers <<-EOC
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test byebug at the end of a class
+         4:    #
+         5:    class #{example_class}
+         6:      def a
+         7:        0
+         8:      end
+         9:
+        10:      byebug
+        11:    end
+        12:  end
+      EOC
+
+      debug_code(program) { assert_equal 11, state.line }
     end
   end
 end