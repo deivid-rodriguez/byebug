81a4f1ba97e4265907e1e77ef0d0ba79e13c8555
David Rodríguez <deivid.rodriguez@gmail.com>
2015-02-19 16:18:13 -0200

    Add some comments

--- lib/byebug/commands/eval.rb
+++ lib/byebug/commands/eval.rb
@@ -6,11 +6,21 @@ module Byebug
   # Utilities used by the eval command
   #
   module EvalFunctions
+    #
+    # Get current binding and yield it to the given block
+    #
     def run_with_binding
       binding = get_binding
       yield binding
     end
 
+    #
+    # Evaluate +expression+ using +binding+
+    #
+    # @param binding [Binding] Context where to evaluate the expression
+    # @param expression [String] Expression to evaluation
+    # @param stack_on_error [Boolean] Whether to show a stack trace on error.
+    #
     def eval_with_setting(binding, expression, stack_on_error)
       return bb_warning_eval(expression, binding) unless stack_on_error
 
