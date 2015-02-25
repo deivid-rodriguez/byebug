deivid-rodriguez/byebug

commit 88451b5a8cff0934a74d7ce781c70ac467b24771

Closes #85

ext/byebug/byebug.c
@@ -263,6 +263,13 @@ call_at_line_check(VALUE context_obj, debug_context_t * dc, VALUE breakpoint,
 /* TracePoint API event handlers */
 
 static void
+update_frame_dest(debug_context_t * dc)
+{
+  dc->dest_frame = dc->calced_stack_size;
+  CTX_FL_UNSET(dc, CTX_FL_IGNORE_STEPS);
+}
+
+static void
 line_event(VALUE trace_point, void *data)
 {
   VALUE breakpoint, file, line, binding, self;
@@ -281,14 +288,15 @@ line_event(VALUE trace_point, void *data)
   if (RTEST(tracing))
     call_at_tracing(context, dc, file, line);
 
-  dc->steps = dc->steps <= 0 ? -1 : dc->steps - 1;
+  if (!CTX_FL_TEST(dc, CTX_FL_IGNORE_STEPS))
+  {
+    dc->steps = dc->steps <= 0 ? -1 : dc->steps - 1;
+  }
+
   if (dc->calced_stack_size <= dc->dest_frame)
   {
+    update_frame_dest(dc);
     dc->lines = dc->lines <= 0 ? -1 : dc->lines - 1;
-    if (dc->calced_stack_size < dc->dest_frame)
-    {
-      dc->dest_frame = dc->calced_stack_size;
-    }
   }
 
   if (dc->steps == 0 || dc->lines == 0
@@ -309,6 +317,11 @@ call_event(VALUE trace_point, void *data)
   VALUE breakpoint, klass, mid, binding, self, file, line;
 
   EVENT_SETUP;
+
+  if (dc->calced_stack_size <= dc->dest_frame)
+  {
+    update_frame_dest(dc);
+  }
 
   dc->calced_stack_size++;
 

test/commands/stepping_test.rb
@@ -163,24 +163,34 @@ module Byebug
          5:    class #{example_class}
          6:      def a
          7:        byebug
-         8:        r = b
+         8:        r = b(c)
          9:        r + 1
         10:      end
         11:
-        12:      def b
+        12:      def b(p)
         13:        r = 2
-        14:        r + 1
+        14:        p + r
         15:      end
-        16:    end
-        17:
-        18:    #{example_class}.new.a
-        19:  end
+        16:
+        17:      def c
+        18:        s = 3
+        19:        s + 2
+        20:      end
+        21:    end
+        22:
+        23:    #{example_class}.new.a
+        24:  end
       EOC
     end
 
     def test_step_then_up_then_next_advances_in_the_upper_frame
       enter 'step', 'up', 'next'
       debug_code(program) { assert_equal 9, state.line }
+    end
+
+    def test_step_then_up_then_steps_in_from_the_upper_frame
+      enter 'step', 'up', 'step'
+      debug_code(program) { assert_equal 13, state.line }
     end
   end
 end

ext/byebug/byebug.h
@@ -14,6 +14,7 @@
 #define CTX_FL_TRACING      (1<<4) /* call at_tracing method                 */
 #define CTX_FL_WAS_RUNNING  (1<<5) /* thread was previously running          */
 #define CTX_FL_STOP_ON_RET  (1<<6) /* can stop on method 'end'               */
+#define CTX_FL_IGNORE_STEPS (1<<7) /* doesn't countdown steps to break       */
 
 /* macro functions */
 #define CTX_FL_TEST(c,f)  ((c)->flags & (f))

ext/byebug/context.c
@@ -385,14 +385,16 @@ Context_stop_reason(VALUE self)
 
 /*
  *  call-seq:
- *    context.step_into(steps)
+ *    context.step_into(steps, frame = 0)
  *
- *  Stops the current context after a number of +steps+ are made.
+ *  Stops the current context after a number of +steps+ are made from frame
+ *  +frame+ (by default the newest one).
  */
 static VALUE
 Context_step_into(int argc, VALUE * argv, VALUE self)
 {
-  VALUE steps;
+  VALUE steps, v_frame;
+  int n_args, from_frame;
   debug_context_t *context;
 
   Data_Get_Struct(self, debug_context_t, context);
@@ -400,11 +402,21 @@ Context_step_into(int argc, VALUE * argv, VALUE self)
   if (context->calced_stack_size == 0)
     rb_raise(rb_eRuntimeError, "No frames collected.");
 
-  rb_scan_args(argc, argv, "10", &steps);
-  if (FIX2INT(steps) < 0)
+  n_args = rb_scan_args(argc, argv, "11", &steps, &v_frame);
+
+  if (FIX2INT(steps) <= 0)
     rb_raise(rb_eRuntimeError, "Steps argument can't be negative.");
 
+  from_frame = n_args == 1 ? 0 : FIX2INT(v_frame);
+
+  if (from_frame < 0 || from_frame >= context->calced_stack_size)
+    rb_raise(rb_eRuntimeError, "Destination frame (%d) is out of range (%d)",
+             from_frame, context->calced_stack_size);
+  else if (from_frame > 0)
+    CTX_FL_SET(context, CTX_FL_IGNORE_STEPS);
+
   context->steps = FIX2INT(steps);
+  context->dest_frame = context->calced_stack_size - from_frame;
 
   return steps;
 }

lib/byebug/commands/stepping.rb
@@ -50,7 +50,7 @@ module Byebug
       steps, err = parse_steps(@match[1], 'Steps')
       return errmsg(err) unless steps
 
-      @state.context.step_into(steps)
+      @state.context.step_into(steps, @state.frame)
       @state.proceed
     end
 

commit a81f889be3a641485aabb8ed1fc25389618f9eae

Fixes regression introduced in 88451b5a
In same cases, `next` would not stop anymore at the next frame after the
current frame finished. Instead, it would wait for a line event in
another frame with the same stack size.

ext/byebug/byebug.c
@@ -228,13 +228,6 @@ call_at_line_check(VALUE context_obj, debug_context_t * dc, VALUE breakpoint,
 /* TracePoint API event handlers */
 
 static void
-update_frame_dest(debug_context_t * dc)
-{
-  dc->dest_frame = dc->calced_stack_size;
-  CTX_FL_UNSET(dc, CTX_FL_IGNORE_STEPS);
-}
-
-static void
 line_event(VALUE trace_point, void *data)
 {
   VALUE brkpnt, file, line, binding;
@@ -253,7 +246,9 @@ line_event(VALUE trace_point, void *data)
 
   if (dc->calced_stack_size <= dc->dest_frame)
   {
-    update_frame_dest(dc);
+    dc->dest_frame = dc->calced_stack_size;
+    CTX_FL_UNSET(dc, CTX_FL_IGNORE_STEPS);
+
     dc->lines = dc->lines <= 0 ? -1 : dc->lines - 1;
   }
 
@@ -279,7 +274,7 @@ call_event(VALUE trace_point, void *data)
   EVENT_SETUP;
 
   if (dc->calced_stack_size <= dc->dest_frame)
-    update_frame_dest(dc);
+    CTX_FL_UNSET(dc, CTX_FL_IGNORE_STEPS);
 
   dc->calced_stack_size++;
 

test/commands/stepping_test.rb
@@ -191,4 +191,51 @@ module Byebug
       debug_code(program) { assert_equal 13, state.line }
     end
   end
+
+  #
+  # Tests next when execution should not stop at the same "stack size level"
+  #
+  class TestNextGoingUpFrames < TestCase
+    def program
+      strip_line_numbers <<-EOC
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test cases where next should not stay in frame
+         4:    #
+         5:    class #{example_class}
+         6:      def finite_loop
+         7:        n = 0
+         8:        loop do
+         9:          n = inc(n)
+        10:          break if n == 2
+        11:          n = inc(n)
+        12:        end
+        13:      end
+        14:
+        15:      def inc(n)
+        16:        if n == 2
+        17:          n
+        18:        else
+        19:          n + 1
+        20:        end
+        21:      end
+        22:    end
+        23:
+        24:    byebug
+        25:
+        26:    #{example_class}.new.finite_loop
+        27:  end
+      EOC
+    end
+
+    def test_next_goes_up_a_frame_if_current_frame_finishes
+      enter 'cont 19', 'next'
+      debug_code(program) { assert_equal 10, state.line }
+    end
+
+    def test_next_does_not_enter_other_frames_of_the_same_size
+      enter 'b 19', 'cont', 'cont', 'next'
+      debug_code(program) { assert_equal 9, state.line }
+    end
+  end
 end