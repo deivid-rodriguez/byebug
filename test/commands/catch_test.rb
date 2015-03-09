8093a118c2b10f1cf249837000bc7df5bdd6e25b
David Rodríguez <deivid.rodriguez@gmail.com>
2015-03-09 17:37:42 -0300

    Some tests for the catch command

---
+++ test/commands/catch_test.rb
@@ -0,0 +1,23 @@
+module Byebug
+  class CatchTestCase < TestCase
+    def program
+      strip_line_numbers <<-EOC
+        1:  byebug
+      EOC
+    end
+
+    def test_catch_removes_specific_catchpoint
+      enter 'catch NoMethodError', 'catch NoMethodError off'
+      debug_code(program)
+
+      assert_empty Byebug.catchpoints
+    end
+
+    def test_catch_off_removes_all_catchpoints_after_confirmation
+      enter 'catch NoMethodError', 'catch off', 'y'
+      debug_code(program)
+
+      assert_empty Byebug.catchpoints
+    end
+  end
+end
