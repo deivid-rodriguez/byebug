d01f2add9679017405d5cf65debcf17fccafcb12
David Rodríguez <deivid.rodriguez@gmail.com>
2015-03-29 16:16:02 -0300

    Improve runner test
    Actually debug program instead of mocking

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -8,6 +8,11 @@ module Byebug
       @runner = Byebug::Runner.new(false)
     end
 
+    def write_debugged_program
+      example_file.write('sleep 0')
+      example_file.close
+    end
+
     def test_run_with_version_flag
       with_command_line('bin/byebug', '--version') { @runner.run }
 
@@ -46,7 +51,7 @@ module Byebug
     end
 
     def test_run_with_a_script_to_debug
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', example_path) do
         @runner.run
@@ -56,7 +61,7 @@ module Byebug
     end
 
     def test_run_with_a_script_and_params_does_not_consume_script_params
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', '--', example_path, '-opt', 'value') do
         @runner.run
@@ -66,7 +71,7 @@ module Byebug
     end
 
     def test_run_with_ruby_script_ruby_is_ignored_and_script_passed_instead
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', '--', 'ruby', example_path) do
         @runner.run
@@ -76,29 +81,31 @@ module Byebug
     end
 
     def test_run_with_no_rc_option
-      @runner.expects(:debug_program)
+      write_debugged_program
       Byebug.expects(:run_init_script).never
 
       with_command_line('bin/byebug', '--no-rc', example_path) { @runner.run }
     end
 
     def test_run_with_post_mortem_mode_flag
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_setting :post_mortem, false do
         with_command_line('bin/byebug', '-m', example_path) do
           @runner.run
+
           assert_equal true, Setting[:post_mortem]
         end
       end
     end
 
     def test_run_with_linetracing_flag
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_setting :linetrace, false do
         with_command_line('bin/byebug', '-t', example_path) do
           @runner.run
+
           assert_equal true, Setting[:linetrace]
         end
       end
@@ -106,7 +113,8 @@ module Byebug
 
     def test_run_with_no_quit_flag
       skip('TODO')
-      @runner.expects(:debug_program)
+      write_debugged_program
+
       with_command_line('bin/byebug', '--no-quit', example_path) do
         @runner.run
 
@@ -115,7 +123,7 @@ module Byebug
     end
 
     def test_run_with_require_flag
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', '-r', 'abbrev', example_path) do
         @runner.run
@@ -126,7 +134,7 @@ module Byebug
     end
 
     def test_run_with_a_single_include_flag
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', '-I', 'dir1', example_path) do
         @runner.run
@@ -136,7 +144,7 @@ module Byebug
     end
 
     def test_run_with_several_include_flags
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', '-I', 'dir1:dir2', example_path) do
         @runner.run
@@ -147,7 +155,7 @@ module Byebug
     end
 
     def test_run_with_debug_flag
-      @runner.expects(:debug_program)
+      write_debugged_program
 
       with_command_line('bin/byebug', '-d', example_path) { @runner.run }
 
@@ -167,8 +175,7 @@ module Byebug
     end
 
     def test_run_successfully
-      example_file.write('sleep 0')
-      example_file.close
+      write_debugged_program
 
       with_command_line('bin/byebug', example_path) { @runner.run }
 
