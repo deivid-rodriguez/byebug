eb31a33dac6591e9218bf7e39599230f0d892925
David Rodríguez <deivid.rodriguez@gmail.com>
2015-07-07 20:45:52 -0300

    Get rid of mocha in `quit` tests

--- test/commands/quit_test.rb
+++ test/commands/quit_test.rb
@@ -1,4 +1,4 @@
-require 'mocha/mini_test'
+require 'minitest/mock'
 require 'test_helper'
 
 module Byebug
@@ -6,46 +6,42 @@ module Byebug
   # Tests exiting Byebug functionality.
   #
   class QuitTestCase < TestCase
-    def test_finishes_byebug_if_user_confirms
-      QuitCommand.any_instance.expects(:exit!)
-      enter 'quit', 'y'
-      debug_code(minimal_program)
+    def faking_exit!
+      Process.stub(:exit!, nil) { yield }
+    end
 
-      check_output_includes 'Really quit? (y/n)'
+    def test_quit_finishes_byebug_if_user_confirms
+      faking_exit! do
+        enter 'quit', 'y'
+        debug_code(minimal_program)
+
+        check_output_includes 'Really quit? (y/n)'
+      end
+    end
+
+    def test_quit_quits_inmediately_if_used_with_bang
+      faking_exit! do
+        enter 'quit!'
+        debug_code(minimal_program)
+
+        check_output_doesnt_include 'Really quit? (y/n)'
+      end
+    end
+
+    def test_quit_quits_inmediately_if_used_with_unconditionally
+      faking_exit! do
+        enter 'quit unconditionally'
+        debug_code(minimal_program)
+
+        check_output_doesnt_include 'Really quit? (y/n)'
+      end
     end
 
     def test_does_not_quit_if_user_did_not_confirm
-      QuitCommand.any_instance.expects(:exit!).never
       enter 'quit', 'n'
       debug_code(minimal_program)
 
       check_output_includes 'Really quit? (y/n)'
-    end
-
-    def test_quits_inmediately_if_used_with_bang
-      QuitCommand.any_instance.expects(:exit!)
-      enter 'quit!'
-      debug_code(minimal_program)
-
-      check_output_doesnt_include 'Really quit? (y/n)'
-    end
-
-    def test_quits_inmediately_if_used_with_unconditionally
-      QuitCommand.any_instance.expects(:exit!)
-      enter 'quit unconditionally'
-      debug_code(minimal_program)
-
-      check_output_doesnt_include 'Really quit? (y/n)'
-    end
-
-    def test_quit_saves_history_and_closes_interface_before_qutting
-      QuitCommand.any_instance.stubs(:exit!)
-
-      interface.expects(:autosave).twice # When exiting REPL and before quitting
-      interface.expects(:close)
-
-      enter 'quit!'
-      debug_code(minimal_program)
     end
   end
 end
