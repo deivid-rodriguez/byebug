8d94030d66d0230880ab4ee7e8d5b859cf017e15
David Rodríguez <deivid.rodriguez@gmail.com>
2015-07-20 08:44:59 -0300

    Remove tests for specific command aliases

--- test/commands/source_test.rb
+++ test/commands/source_test.rb
@@ -32,30 +32,28 @@ module Byebug
       retry
     end
 
-    %w(source so).each do |cmd_alias|
-      define_method(:"test_#{cmd_alias}_runs_byebug_commands_from_file") do
-        enter "#{cmd_alias} source_example.txt"
+    def test_source_runs_byebug_commands_from_file
+      enter 'source source_example.txt'
 
-        debug_code(program) do
-          assert_equal 4, Breakpoint.first.pos
-          assert_equal 5, Breakpoint.last.pos
-          assert_equal 'true', Breakpoint.last.expr
-        end
+      debug_code(program) do
+        assert_equal 4, Breakpoint.first.pos
+        assert_equal 5, Breakpoint.last.pos
+        assert_equal 'true', Breakpoint.last.expr
       end
+    end
 
-      define_method(:"test_#{cmd_alias}_shows_an_error_if_file_not_found") do
-        enter "#{cmd_alias} blabla"
-        debug_code(program)
+    def test_source_shows_an_error_if_file_not_found
+      enter 'source blabla'
+      debug_code(program)
 
-        check_error_includes(/File ".*blabla" not found/)
-      end
+      check_error_includes(/File ".*blabla" not found/)
+    end
 
-      define_method(:"test_#{cmd_alias}_without_arguments_shows_help") do
-        enter 'source'
-        debug_code(program)
+    def test_source_without_arguments_shows_help
+      enter 'source'
+      debug_code(program)
 
-        check_output_includes('Restores a previously saved byebug session')
-      end
+      check_output_includes('Restores a previously saved byebug session')
     end
   end
 end
