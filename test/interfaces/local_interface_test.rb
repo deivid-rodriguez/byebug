6e56b5f6ea4d5270648526df7bda2d29b63abba7
Takashi Kokubun <takashikkbn@gmail.com>
2015-10-14 07:32:50 +0000

    Allow LocalInterface to execute continue by C-d

---
+++ test/interfaces/local_interface_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+require 'minitest/mock'
+
+module Byebug
+  #
+  # Tests the local interface
+  #
+  class LocalInterfaceTest < TestCase
+    def test_continues_by_control_d
+      # `Readline.readline` returns nil for Control-D
+      Readline.stub(:readline, nil) do
+        interface = LocalInterface.new
+        assert_equal 'continue', interface.readline('(byebug)')
+      end
+    end
+  end
+end
