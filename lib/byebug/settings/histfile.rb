023eac2127a2a90c4d14b38499628fbbde82df3d
David Rodríguez <deivid.rodriguez@gmail.com>
2015-11-08 18:53:59 +0100

    Use ".byebug_history" for default history file
    This is just for consistency with bash or gdb.

--- lib/byebug/settings/histfile.rb
+++ lib/byebug/settings/histfile.rb
@@ -5,10 +5,10 @@ module Byebug
   # Setting to customize the file where byebug's history is saved.
   #
   class HistfileSetting < Setting
-    DEFAULT = File.expand_path('.byebug_hist')
+    DEFAULT = File.expand_path('.byebug_history')
 
     def banner
-      'File where cmd history is saved to. Default: ./.byebug_hist'
+      'File where cmd history is saved to. Default: ./.byebug_history'
     end
 
     def to_s
