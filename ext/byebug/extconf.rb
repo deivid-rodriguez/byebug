bea29fac443fd35635549683a4865309336d9034
David Rodríguez <deivid.rodriguez@gmail.com>
2016-01-09 14:44:44 +0100

    Fix clang 2.3.0 build

--- ext/byebug/extconf.rb
+++ ext/byebug/extconf.rb
@@ -14,6 +14,7 @@ makefile_config['CFLAGS'] << ' -gdwarf-2 -g3 -O0' if ENV['debug']
 
 if makefile_config['CC'] =~ /clang/
   makefile_config['CFLAGS'] << ' -Wno-unknown-warning-option'
+  makefile_config['CFLAGS'] << ' -Wno-ignored-attributes'
 end
 
 dir_config('ruby')
