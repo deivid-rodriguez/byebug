2e621eca4aec3491759e374333fda173346a101f
David Rodríguez <deivid.rodriguez@gmail.com>
2016-02-02 22:54:05 +0100

    Temporarily override Ctrl-C handling
    We want full control of interrupts while in byebug's prompt. For example,
    we don't want to exit when stopped inside a spec, but rather reset the
    command line and give back another prompt to the user, as usual.

--- lib/byebug/interfaces/local_interface.rb
+++ lib/byebug/interfaces/local_interface.rb
@@ -14,17 +14,32 @@ module Byebug
     end
 
     #
-    # Reads a single line of input using Readline. If Ctrl-C is pressed in the
-    # middle of input, the line is reset to only the prompt and we ask for input
-    # again. If Ctrl-D is pressed, it returns "continue".
+    # Reads a single line of input using Readline. If Ctrl-D is pressed, it
+    # returns "continue", meaning that program's execution will go on.
     #
     # @param prompt Prompt to be displayed.
     #
     def readline(prompt)
-      Readline.readline(prompt, false) || EOF_ALIAS
+      with_repl_like_sigint do
+        Readline.readline(prompt, false) || EOF_ALIAS
+      end
+    end
+
+    #
+    # Yields the block handling Ctrl-C the following way: if pressed while
+    # waiting for input, the line is reset to only the prompt and we ask for
+    # input again.
+    #
+    # @note Any external 'INT' traps are overriden during this method.
+    #
+    def with_repl_like_sigint
+      orig_handler = trap('INT') { raise Interrupt }
+      yield
     rescue Interrupt
       puts('^C')
       retry
+    ensure
+      trap('INT', orig_handler)
     end
   end
 end
