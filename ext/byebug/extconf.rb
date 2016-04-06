34bdbeb5104ad0ba97902077a1aa50a4e575efcc
David Rodríguez <deivid.rodriguez@gmail.com>
2016-04-06 17:07:40 +0200

    Keep default compilation flags
    It's better to keep the default compilation flags (that were most likely
    used to compile Ruby in the target system). The `-Werror` flag is
    specially harmful since I think it's been preventing successful
    installation on some systems (Windows). I've been getting reports about
    failed installations in Windows (#175, #226) where installation fails on
    some warnings on Ruby header files. I think removing this flag should
    allow `byebug` to be properly installed on those systems. Also, remove
    a couple of specific flags for clang compilations, since we no longer
    care for a warning free compilation.

--- ext/byebug/extconf.rb
+++ ext/byebug/extconf.rb
@@ -4,13 +4,7 @@ makefile_config = RbConfig::MAKEFILE_CONFIG
 
 makefile_config['CC'] = ENV['CC'] if ENV['CC']
 
-makefile_config['CFLAGS'] << ' -Wall -Werror'
 makefile_config['CFLAGS'] << ' -gdwarf-2 -g3 -O0' if ENV['debug']
-
-if makefile_config['CC'] =~ /clang/
-  makefile_config['CFLAGS'] << ' -Wno-unknown-warning-option'
-  makefile_config['CFLAGS'] << ' -Wno-ignored-attributes'
-end
 
 dir_config('ruby')
 with_cflags(makefile_config['CFLAGS']) { create_makefile('byebug/byebug') }
