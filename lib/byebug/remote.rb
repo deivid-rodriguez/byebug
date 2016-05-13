32c923f37ef86596faa157429b3f7466ad1aae40
David Rodríguez <deivid.rodriguez@gmail.com>
2016-05-13 10:22:05 +0200

    Remove unnecessary throw-catch mechanism

--- lib/byebug/remote.rb
+++ lib/byebug/remote.rb
@@ -79,20 +79,18 @@ module Byebug
       socket = TCPSocket.new(host, port)
       puts 'Connected.'
 
-      catch(:exit) do
-        while (line = socket.gets)
-          case line
-          when /^PROMPT (.*)$/
-            input = Context.interface.read_command(Regexp.last_match[1])
-            throw :exit unless input
-            socket.puts input
-          when /^CONFIRM (.*)$/
-            input = Context.interface.confirm(Regexp.last_match[1])
-            throw :exit unless input
-            socket.puts input
-          else
-            puts line
-          end
+      while (line = socket.gets)
+        case line
+        when /^PROMPT (.*)$/
+          input = Context.interface.read_command(Regexp.last_match[1])
+          break unless input
+          socket.puts input
+        when /^CONFIRM (.*)$/
+          input = Context.interface.confirm(Regexp.last_match[1])
+          break unless input
+          socket.puts input
+        else
+          puts line
         end
       end
       socket.close
