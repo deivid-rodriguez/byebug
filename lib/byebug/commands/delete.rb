1e18f2a14ed39604f14e8a8693abd84bcc7291ad
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-09 13:05:49 -0300

    Add a message on deleting a breakpoint

--- lib/byebug/commands/delete.rb
+++ lib/byebug/commands/delete.rb
@@ -44,7 +44,9 @@ module Byebug
 
         return errmsg(err) unless pos
 
-        unless Breakpoint.remove(pos)
+        if Breakpoint.remove(pos)
+          puts(pr('break.messages.breakpoint_deleted', pos: pos))
+        else
           errmsg(pr('break.errors.no_breakpoint_delete', pos: pos))
         end
       end
