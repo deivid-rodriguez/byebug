cc9453d2d4e286b8e7a37fea9fade021101efaf2
Tacnoman Cassino <renatocassino@gmail.com>
2017-11-15 16:28:41 -0200

    Command to run always

--- lib/byebug/commands/continue.rb
+++ lib/byebug/commands/continue.rb
@@ -20,9 +20,7 @@ module Byebug
     def self.description
       <<-DESCRIPTION
         c[ont[inue]][ <line_number>]
-
         #{short_description}
-
         Normally the program stops at the next breakpoint. However, if the
         parameter "unconditionally" is given or the command is suffixed with
         "!", the program will run until the end regardless of any enabled
