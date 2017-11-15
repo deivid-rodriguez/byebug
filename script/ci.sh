037214d93850657d52046ed716114d1cd59614da
David Rodríguez <deivid.rodriguez@riseup.net>
2017-11-15 08:15:03 -0300

    Bad times for bundler & rubygems

--- script/ci.sh
+++ script/ci.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-gem update --system --no-document
-gem install bundler --no-document
 bundle install --jobs 3 --retry 3
 bundle exec rake clobber compile test sign_hooks overcommit
