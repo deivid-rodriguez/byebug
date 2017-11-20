1255df89de3acd0c2b1b09f0f7433e5130966733
Cassino <renato.santos@corp.globo.com>
2017-11-20 14:00:45 -0200

    Adding tests to continue breakpoint and continue always

---
+++ test/commands/continue_always_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+module Byebug
+  #
+  # Tests for continue command
+  #
+  class ContinueAlwaysTest < TestCase
+    def program
+      strip_line_numbers <<-RUBY
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test continue command.
+         4:    #
+         5:    class #{example_class}
+         6:      def factor(num)
+         7:        i=1
+         8:        num.times do |new_number|
+         9:          i*= new_number
+        10:          byebug
+        11:        end
+        12:      end
+        13:    end
+        14:    c = 5
+        15:
+        16:    result = #{example_class}.new.factor(c)
+        17:    
+        18:    "Result is: " + result.to_s
+        19:  end
+      RUBY
+    end
+
+    def reset_commands
+      ContinueAlwaysCommand.always_run = 0
+      ListCommand.always_run = 1
+    end
+
+    def test_continues_and_never_stop_again
+      enter 'continue_always'
+
+      debug_code(program) { assert_program_finished }
+      reset_commands
+    end
+
+    def test_continues_and_never_stop_again_using_abbreviation
+      enter 'ca'
+
+      debug_code(program) { assert_program_finished }
+      reset_commands
+    end
+
+    def test_continues_and_never_stop_again_using_another_abbreviation
+      enter 'cont_always'
+
+      debug_code(program) { assert_program_finished }
+      reset_commands
+    end
+  end
+end
+
---
+++ test/commands/continue_breakpoint_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+module Byebug
+  #
+  # Tests for continue command
+  #
+  class ContinueBreakpointTest < TestCase
+    def program
+      strip_line_numbers <<-RUBY
+         1:  module Byebug
+         2:    #
+         3:    # Toy class to test continue command.
+         4:    #
+         5:    class #{example_class}
+         6:      def factor(num)
+         7:        i=1
+         8:        num.times do |new_number|
+         9:          i*= new_number
+        10:          byebug
+        11:        end
+        12:      end
+        13:    end
+        14:    c = 5
+        15:
+        16:    result = #{example_class}.new.factor(c)
+        17:    byebug
+        18:    "Result is: " + result.to_s
+        19:  end
+      RUBY
+    end
+
+    def test_continues_until_the_end_if_no_line_specified_and_no_breakpoints
+      enter 'continue_breakpoint'
+
+      debug_code(program) { assert_location example_path, 18 }
+    end
+
+    def test_works_in_abbreviated_mode_too
+      enter 'cb'
+
+      debug_code(program) { assert_location example_path, 18 }
+    end
+  end
+end
