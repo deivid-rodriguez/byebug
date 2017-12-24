82df3083fc22af3cdc324779e9e87ef7567acedc
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-24 14:26:35 -0300

    Use latest rubygems too

--- script/ci.sh
+++ script/ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+gem update --system 2.7.3
 gem install bundler -v 1.16.1 --no-document --conservative
 bundle install --jobs 3 --retry 3 --path .bundle/gems
 bundle exec rake clobber compile test sign_hooks overcommit
