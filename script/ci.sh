6397ec99bfa9244dbd68707e8b0b1745f5deec02
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-24 15:55:20 -0300

    More random testing

--- script/ci.sh
+++ script/ci.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-gem uninstall bundler --force --executables
-gem update --system 2.7.3
 bundle install --jobs 3 --retry 3 --path .bundle/gems
 bundle exec rake clobber compile test sign_hooks overcommit
