b74b6741bf7c03e32444793a2bd3135ac71bb5ff
David Rodríguez <deivid.rodriguez@riseup.net>
2020-04-11 13:05:55 +0200

    Stop tracking coverage on newer rubies
    Ruby's coverage library no longer supports `TracePoint` events :(
    
    See https://bugs.ruby-lang.org/issues/16776.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "simplecov" if ENV["NOCOV"].nil?
+require "simplecov" if ENV["NOCOV"].nil? && Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.6.a")
 require "support/test_case"
 
 Byebug::TestCase.before_suite
