7b6a626639d6fd0a7bd8f44689e539b814d135fc
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2025-10-03 17:51:00 +0200

    Simple repro

--- repl.rb
+++ repl.rb
@@ -3,14 +3,4 @@
 
 require "readline"
 
-def with_repl_like_sigint
-  orig_handler = trap("INT") { raise Interrupt }
-  yield
-rescue Interrupt
-  puts("^C")
-  retry
-ensure
-  trap("INT", orig_handler)
-end
-
-with_repl_like_sigint { Readline.readline("foo") }
+Readline.readline("foo")
